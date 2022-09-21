e2f71cd9e573b5aca58819da9ed11216410366d8
Connor McCue <connor.mccue@comcast.net>
2022-09-21 16:48:57 -0700

    fixed multiple pokecards

---
+++ src/Components/Controls/Select.js
@@ -0,0 +1,14 @@
+const options: ["bug", "flying", "water"]
+
+export default function Select({ options, changeHandler }) {
+  return (
+    <select onChange={(e) => changeHandler(e.target.value)}>
+      <option value="all"> all </option>
+      {options.map((opt) => (
+        <option key={opt} value={opt}>
+          {opt}
+        </option>
+      ))}
+    </select>
+  );
+}
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,27 +4,12 @@ import './Pokedex.css';
 
 export default function Pokedex() {
   const { pokemon } = usePokemon();
-  let { pokemon2 } = pokemon.map((pkm) => 
-  {if (pkm.species_id.length === 1) {
-    pkm.species_id = '00' + pkm.species_id
-  }
-  if (pkm.species_id.length === 2) {
-    pkm.species_id = '0' + pkm.species_id
-  } 
-  return pokemon;);
-  console.log(pokemon2);
-
   return (
     <div className="pokeContainer">
       {pokemon.map((pkmn) => (
         <div className="pokeCard" key={pkmn.id}> 
           <div>
-            {pkmn.species_id.length === 3} 
-            <img src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/${pkmn.species_id}.png`}/>
-            {pkmn.species_id.length === 2} 
-            <img src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/0${pkmn.species_id}.png`}/>
-            {pkmn.species_id.length === 1}  
-            <img src={`http://assets.pokemon.com/assets/cms2/img/pokedex/full/00${pkmn.species_id}.png`}/>
+            <img src={`${pkmn.url_image}`} />
           </div>
           <h1>{pkmn.pokemon}</h1>
           <h4>{pkmn.type_1}</h4>
