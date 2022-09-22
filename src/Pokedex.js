40b25f6b8ca76d8247e751123c634d0e80b1ac84
Connor McCue <connor.mccue@comcast.net>
2022-09-21 17:12:16 -0700

    css 2.0

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -9,18 +9,19 @@ export default function Pokedex() {
       {pokemon.map((pkmn) => (
         <div className="pokeCard" key={pkmn.id}> 
           <div>
-            <img src={`${pkmn.url_image}`} />
+            <img src={`${pkmn.url_image}`} className="pokeImage"/>
           </div>
           <h1>{pkmn.pokemon}</h1>
-          <div className="type"> Type:
-            <h4>{pkmn.type_1}</h4>
-            <h4>{pkmn.type_2}</h4>
+          <h4>Type:</h4>
+          <div className="type">
+            <p> {pkmn.type_1}, {pkmn.type_2}</p>
           </div>
-          <div className="stats"> Stats:
-            <p>Height {pkmn.height}</p>
-            <p>Weight {pkmn.weight}</p>
-            <p>Attack {pkmn.attack}</p>
-            <p>Defense {pkmn.defense}</p>
+          <h4>Stats:</h4>
+          <div className="stats"> 
+            <p>Height: {pkmn.height}</p>
+            <p>Weight: {pkmn.weight}</p>
+            <p>Attack: {pkmn.attack}</p>
+            <p>Defense: {pkmn.defense}</p>
           </div>
         </div>
       ))}
